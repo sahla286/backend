180666bed291caf9a6871fa77b1019680b4b9e73
sahla286 <tcsahla@gmail.com>
2023-12-04 15:02:04 +0530

    updt

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,45 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-  logout,
-  getUser,
-  getLoginStatus,
-  updateUser,
-  addToWishlist,
-  removeFromWishlist,
-  getWishlist,
-  getCart,
-  saveCart,
-  clearCart,
-  updatePhoto,
-  // changePassword,
-  // forgotPassword,
-  // resetPassword,
-} = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
-// comment
+const { registerUser,loginUser ,logout, getUser, getLoginStatus, updateUser, updatePhoto}= require("../controllers/userController");
+const {protect}= require("../middleware/authMiddleware");
+const { getLogger } = require("nodemailer/lib/shared");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/logout", logout);
-router.get("/getUser", protect, getUser);
-router.get("/getLoginStatus", getLoginStatus);
-router.patch("/updateUser", protect, updateUser);
-router.patch("/updatePhoto", protect, updatePhoto);
-// router.patch("/changepassword", protect, changePassword);
-// router.post("/forgotpassword", forgotPassword);
-// router.put("/resetpassword/:resetToken", resetPassword);
-
-// wishlist
-router.post("/addToWishlist", protect, addToWishlist);
-router.get("/getWishlist", protect, getWishlist);
-router.put("/wishlist/:productId", protect, removeFromWishlist);
-
-// cart
-router.get("/getCart", protect, getCart);
-router.patch("/saveCart", protect, saveCart);
-router.patch("/clearCart", protect, clearCart);
-
+router.post("/register",registerUser);
+router.post("/login",loginUser);
+router.get("/logout",logout);
+router.get("/getUser",protect,getUser);
+router.get("/getLoginStatus",getLoginStatus);
+router.patch("/updateUser",protect,updateUser);
+router.patch("/updatePhoto",protect,updatePhoto);
 module.exports = router;
